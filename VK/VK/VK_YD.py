ca49d1652d947735cb69c3b713a626a8f5b256ef
Anton Ryabenko <djedra@yandex.ru>
2024-02-07 14:22:48 +0700

    init2

--- VK/VK/VK_YD.py
+++ VK/VK/VK_YD.py
@@ -1,31 +1,28 @@
 import requests
-import time
 import configparser
 from tqdm import tqdm
 from datetime import datetime
 import os
 
-
-config = configparser.ConfigParser()  # объекта парсера
+config = configparser.ConfigParser()
 config.read('settings.ini')
 
 VKtoken = config["VK"]["token"]
 YAtoken = config["Yandex"]["token"]
 VK_id = config["VK"]["id_vk"]
 
-class VkUser:
 
+class VkUser:
     def __init__(self, token, version):
-        self.params = {
-            'access_token': token,
-            'v': version
-        }
+        self.token = token
+        self.version = version
+
     def get_photo(self):
         url = 'https://api.vk.com/method/photos.get'
         params = {
-            'user_id': VK_id,
-            'access_token': VKtoken,
-            'v': '5.131',
+            'owner_id': VK_id,
+            'access_token': self.token,
+            'v': self.version,
             'extended': '1',
             'album_id': 'wall',
             'photo_sizes': '1',
@@ -36,46 +33,22 @@ class VkUser:
         req = requests.get(url, params=params).json()
 
         dict_photo = {}
-        for dict_ in (req['response']['items']):
-            for i in dict_['sizes']:
-                if i['type'] == 'z':
-                    j = dict_['likes']
-                    p = j['count']
-
-                    if dict_photo.keys() == p:
-                        dict_photo[p + current_datetime] = i['url']
-                    else:
-                        dict_photo[p] = i['url']
+        for item in (req['response']['items']):
+            likes = item['likes']['count']
+            for size in item['sizes']:
+                if size['type'] == 'z':
+                    dict_photo[likes] = size['url']
         return dict_photo
 
-    def get_file(self):
-        url = vk.get_photo().values()
+    def download_photo(self, likes, url):
+        filename = f'VK_photo/{likes}_{current_datetime}.jpg'
         r = requests.get(url)
-        with open('VK_photo/' + vk.get_photo().values() + '.jpg', 'wb') as f:
+        with open(filename, 'wb') as f:
             f.write(r.content)
-
-current_datetime = datetime.now().now().strftime('%Y-%m-%d_%H-%M-%S')
-
-vk = VkUser(VKtoken, '5.131')
-
-path1 = 'VK_photo/'
-if path1 in os.getcwd():
-    for i, j in vk.get_photo().items():
-        r = requests.get(j)
-        with open(path1 + str(i) + '.jpg', 'wb') as f:
-            f.write(r.content)
-if not os.path.isdir('VK_photo'):
-    os.mkdir('VK_photo')
-    for i, j in vk.get_photo().items():
-        r = requests.get(j)
-        with open(path1 + str(i) + '.jpg', 'wb') as f:
-            f.write(r.content)
-
-
+        return filename
 
 
 class YandexDisk:
-
     def __init__(self, token):
         self.token = token
 
@@ -85,14 +58,14 @@ class YandexDisk:
             'Authorization': 'OAuth {}'.format(self.token)
         }
 
-    def _get_path(self, disk_file_path):
+    def _create_folder(self, disk_folder_path):
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources"
         headers = self.get_headers()
-        response = requests.put(f'{upload_url}?path={disk_file_path}', headers=headers)
+        response = requests.put(f'{upload_url}?path={disk_folder_path}', headers=headers)
         if response.status_code == 201:
-            print('Sucsess')
+            print('Folder created successfully')
 
-    def _get_upload_link(self, disk_file_path):
+    def get_upload_link(self, disk_file_path):
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = self.get_headers()
         params = {"path": disk_file_path, "overwrite": "true"}
@@ -100,22 +73,33 @@ class YandexDisk:
         return response.json()
 
     def upload_file_to_disk(self, disk_file_path, filename):
-        href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
+        href = self.get_upload_link(disk_file_path=disk_file_path).get("href", "")
         response = requests.put(href, data=open(filename, 'rb'))
         response.raise_for_status()
 
 
+def download_and_save_photos(photo_dict):
+    vk = VkUser(VKtoken, '5.131')
+    global current_datetime
+    current_datetime = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
-ya = YandexDisk(token=YAtoken)
+    if not os.path.isdir('VK_photo'):
+        os.mkdir('VK_photo')
 
-path = 'backUP_photo_VK_'
+    ya = YandexDisk(token=YAtoken)
+    path = f'backUP_photo_VK_{current_datetime}'
+    ya._create_folder(path)
 
-ya._get_path(path)
-ya._get_upload_link(path)
-
-for i in os.walk('VK_photo/'):
-    for j in tqdm(i[2]):
-        time.sleep(0.5)
-        ya.upload_file_to_disk('backUP_photo_VK_/' + str(j), 'VK_photo/' + str(j))
+    for likes, url in tqdm(photo_dict.items(), desc="Downloading photos"):
+        filename = vk.download_photo(likes, url)
+        ya.upload_file_to_disk(f"{path}/{os.path.basename(filename)}", filename)
 
 
+def main():
+    vk = VkUser(VKtoken, '5.131')
+    photo_dict = vk.get_photo()
+    download_and_save_photos(photo_dict)
+
+
+if __name__ == "__main__":
+    main()
